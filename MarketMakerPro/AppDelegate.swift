d6f444c8818abad12ada47a7136ae71ca31f24c3
Andy Alleyne <andyalleyne@andys-macbook-pro.local>
2018-12-15 08:03:21 -0500

    all pods and AdMobs installed with 4 bannerAds

--- MarketMakerPro/AppDelegate.swift
+++ MarketMakerPro/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //Firebase setup
+        FirebaseApp.configure()
+        //ADMob setup
+        GADMobileAds.configure(withApplicationID: "ca-app-pub-7563192023707820~5820798002")
         return true
     }
 
